f1623727c3cdc5b9b064ec9f4ef994b5d87aee26
RobinPbt <104992181+RobinPbt@users.noreply.github.com>
2024-03-27 17:40:04 +0100

    Add new tables ratings and stock information

--- airflow-docker/dags/update_month.py
+++ airflow-docker/dags/update_month.py
@@ -33,6 +33,8 @@ from functions import *
 )
 def update_db_monthly():
   
+    # General information tables
+    
     create_general_information_table = PostgresOperator(
         task_id="create_general_information_table",
         postgres_conn_id="finapp_postgres_conn",
@@ -55,12 +57,12 @@ def update_db_monthly():
             );""",
     )
 
-    create_temp_info_table = PostgresOperator(
-        task_id="create_temp_info_table",
+    create_temp_general_info_table = PostgresOperator(
+        task_id="create_temp_general_info_table",
         postgres_conn_id="finapp_postgres_conn",
         sql="""
-            DROP TABLE IF EXISTS temp_info;
-            CREATE TABLE temp_info (
+            DROP TABLE IF EXISTS temp_general_info;
+            CREATE TABLE temp_general_info (
                 "symbol" TEXT,
                 "date" TIMESTAMP,
                 "sector" TEXT,
@@ -78,6 +80,88 @@ def update_db_monthly():
             );""",
     )
 
+    # Stock information tables
+
+    create_stock_information_table = PostgresOperator(
+        task_id="create_stock_information_table",
+        postgres_conn_id="finapp_postgres_conn",
+        sql="""
+            CREATE TABLE IF NOT EXISTS stock_information (
+                "symbol" TEXT,
+                "date" TIMESTAMP,
+                "floatShares" BIGINT,
+                "sharesOutstanding" BIGINT,
+                "heldPercentInsiders" FLOAT,
+                "heldPercentInstitutions" FLOAT,
+                "lastSplitFactor" TEXT,
+                "lastSplitDate" TIMESTAMP
+            );""",
+    )
+
+    create_temp_stock_info_table = PostgresOperator(
+        task_id="create_temp_stock_info_table",
+        postgres_conn_id="finapp_postgres_conn",
+        sql="""
+            DROP TABLE IF EXISTS temp_stock_info;
+            CREATE TABLE temp_stock_info (
+                "symbol" TEXT,
+                "date" TIMESTAMP,
+                "floatShares" BIGINT,
+                "sharesOutstanding" BIGINT,
+                "heldPercentInsiders" FLOAT,
+                "heldPercentInstitutions" FLOAT,
+                "lastSplitFactor" TEXT,
+                "lastSplitDate" TIMESTAMP
+            );""",
+    )
+
+    # Ratings tables
+
+    create_ratings_table = PostgresOperator(
+        task_id="create_ratings_table",
+        postgres_conn_id="finapp_postgres_conn",
+        sql="""
+            CREATE TABLE IF NOT EXISTS ratings (
+                "symbol" TEXT,
+                "date" TIMESTAMP,
+                "auditRisk" INT,
+                "boardRisk" INT,
+                "compensationRisk" INT,
+                "shareHolderRightsRisk" INT,
+                "overallRisk" INT,
+                "totalEsg" FLOAT,
+                "environmentScore" FLOAT,
+                "socialScore" FLOAT,
+                "governanceScore" FLOAT,
+                "ratingYear" INT,
+                "ratingMonth" INT,
+                "highestControversy" FLOAT
+            );""",
+    )
+
+    create_temp_ratings_table = PostgresOperator(
+        task_id="create_temp_ratings_table",
+        postgres_conn_id="finapp_postgres_conn",
+        sql="""
+            DROP TABLE IF EXISTS temp_ratings;
+            CREATE TABLE temp_ratings (
+                "symbol" TEXT,
+                "date" TIMESTAMP,
+                "auditRisk" INT,
+                "boardRisk" INT,
+                "compensationRisk" INT,
+                "shareHolderRightsRisk" INT,
+                "overallRisk" INT,
+                "totalEsg" FLOAT,
+                "environmentScore" FLOAT,
+                "socialScore" FLOAT,
+                "governanceScore" FLOAT,
+                "ratingYear" INT,
+                "ratingMonth" INT,
+                "highestControversy" FLOAT
+            );""",
+    )
+
     @task(task_id="download_data")
     def download_data():
 
@@ -88,10 +172,11 @@ def update_db_monthly():
         list_tickers = [ticker, ticker2, ticker3]
         tickers = Ticker(list_tickers)
 
-        # Request API and create a DataFrame
+        # Get current time (CET timezone)
         timezone = pytz.timezone('CET')
         query_time = dt.datetime.now(tz=timezone).replace(microsecond=0)
-        
+
+        # Request API to get general informations        
         query_result_1 = tickers.asset_profile
         selected_items_1 = ['sector', 'industry', 'country', 'fullTimeEmployees']
 
@@ -106,14 +191,37 @@ def update_db_monthly():
         # Cast fullTimeEmployees in INT, impossible since NaN is incompatbale with INT type
         # general_information['fullTimeEmployees'] = general_information['fullTimeEmployees'].apply(lambda x : int(x) if not np.isnan(x) else np.NaN)
 
-        # Save results in csv file
+        # Request API to get stock informations
+        query_result = tickers.key_stats
+        selected_items = ['floatShares', 'sharesOutstanding', 'heldPercentInsiders', 'heldPercentInstitutions', 'lastSplitFactor', 'lastSplitDate']
+        stock_information = extract_data_single(query_result, selected_items, query_time=query_time)
+
+        # Request API to get ratings
+        query_result_1 = tickers.asset_profile
+        selected_items_1 = ['auditRisk', 'boardRisk', 'compensationRisk', 'shareHolderRightsRisk', 'overallRisk']
+
+        query_result_2 = tickers.esg_scores
+        selected_items_2 = ['totalEsg', 'environmentScore', 'socialScore', 'governanceScore', 'ratingYear', 'ratingMonth', 'highestControversy']
+
+        query_result_list = [query_result_1, query_result_2]
+        selected_items_list = [selected_items_1, selected_items_2]
+
+        ratings = extract_data_multiple(query_result_list, selected_items_list, query_time=query_time)
+
+        # Save results in csv files
         files_dir_path = "/opt/airflow/dags/files/"
 
         if not os.path.exists(os.path.exists(files_dir_path)):
             os.mkdir(files_dir_path)
 
-        file_path = os.path.join(files_dir_path, "general_information.csv")
-        general_information.to_csv(file_path, index=False)
+        general_file_path = os.path.join(files_dir_path, "general_information.csv")
+        general_information.to_csv(general_file_path, index=False)
+
+        stock_file_path = os.path.join(files_dir_path, "stock_information.csv")
+        stock_information.to_csv(stock_file_path, index=False)
+
+        ratings_file_path = os.path.join(files_dir_path, "ratings.csv")
+        ratings.to_csv(ratings_file_path, index=False)
     
     @task(task_id="update_db")
     def update_db():       
@@ -124,28 +232,62 @@ def update_db_monthly():
         cur = conn.cursor()
         
         # Copy the files previously saved in tables
-        prices_file_path = "/opt/airflow/dags/files/general_information.csv"
-        with open(prices_file_path, "r") as file:
+        general_file_path = "/opt/airflow/dags/files/general_information.csv"
+        with open(general_file_path, "r") as file:
             cur.copy_expert(
-                "COPY temp_info FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                "COPY temp_general_info FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                file,
+            )
+
+        stock_file_path = "/opt/airflow/dags/files/stock_information.csv"
+        with open(stock_file_path, "r") as file:
+            cur.copy_expert(
+                "COPY temp_stock_info FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+                file,
+            )
+
+        ratings_file_path = "/opt/airflow/dags/files/ratings.csv"
+        with open(ratings_file_path, "r") as file:
+            cur.copy_expert(
+                "COPY temp_ratings FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
                 file,
             )
         
         conn.commit()
 
-        # Insert the result of the request in the table stock_price_minute
-        query_1 = """
+        # Insert the result of the request in the table permanent table
+        query_general_information_1 = """
             INSERT INTO general_information
             SELECT *
-            FROM temp_info;
+            FROM temp_general_info;
         """
 
-        query_2 = """DROP TABLE IF EXISTS temp_info;"""
+        query_general_information_2 = """DROP TABLE IF EXISTS temp_general_info;"""
 
-        cur.execute(query_1)
-        cur.execute(query_2)
+        query_stock_information_1 = """
+            INSERT INTO stock_information
+            SELECT *
+            FROM temp_stock_info;
+        """
+
+        query_stock_information_2 = """DROP TABLE IF EXISTS temp_stock_info;"""
+
+        query_ratings_1 = """
+            INSERT INTO ratings
+            SELECT *
+            FROM temp_ratings;
+        """
+
+        query_ratings_2 = """DROP TABLE IF EXISTS temp_ratings;"""
+
+        cur.execute(query_general_information_1)
+        cur.execute(query_general_information_2)
+        cur.execute(query_stock_information_1)
+        cur.execute(query_stock_information_2)
+        cur.execute(query_ratings_1)
+        cur.execute(query_ratings_2)
         conn.commit()
 
-    [create_general_information_table, create_temp_info_table] >> download_data() >> update_db()
+    [create_general_information_table, create_temp_general_info_table, create_stock_information_table, create_temp_stock_info_table, create_ratings_table, create_temp_ratings_table] >> download_data() >> update_db()
 
 dag = update_db_monthly()
