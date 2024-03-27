fe4216ddca31ccc406c15ab8fab8f9810b4c7fb4
RobinPbt <104992181+RobinPbt@users.noreply.github.com>
2024-03-27 12:15:34 +0100

    Externalize functions

---
+++ airflow-docker/plugins/functions.py
@@ -0,0 +1,94 @@
+from yahooquery import Ticker
+import pandas as pd
+import numpy as np
+import datetime as dt
+import pytz
+
+def extract_data_single(query_result, selected_items, query_time=None):
+    """
+    Extracts items from a query with yahooquery library. 
+    Returns a list with one dict per ticker in the query containing query_time and selected_items datas 
+       
+    Arguments
+    ----------
+    query_result: dict
+        Result of a query with yahooquery library with a method applied (ex: yahooquery.Ticker(ticker).financial_data)
+    selected_items: list of str
+        Names of items to extract (ex: ['regularMarketPrice', 'regularMarketDayHigh'])
+    query_time: timestamp or None
+        Time at which the query has been performed, adds a column with this information for each row in the query
+    """
+    
+    tickers = list(query_result.keys())   
+    results_list = []
+    
+    # For each ticker extract selected items
+    for ticker in tickers:
+        
+        # Get query result for the current ticker
+        query_result_ticker = query_result[ticker]
+        
+        # Instantiante result with time and ticker
+        if query_time:
+            ticker_result = {'symbol': ticker, 'date': query_time}
+        else:
+            ticker_result = {'symbol': ticker}
+        
+        # Collect name of available items for the ticker (yahooquery doesn't return the same items depending on the ticker)
+        if isinstance(query_result_ticker, str): # If the query doesn't find any results it resturns a string
+            available_items = []
+        
+        else: # Else get names of items returned
+            available_items = query_result_ticker.keys()
+        
+        # Now extract items if available
+        for item in selected_items:
+        
+            # Check if data is available, and append items
+            if item in available_items:
+                ticker_result[item] = query_result_ticker[item]
+
+            # If not available, fill with NaN
+            else:
+                ticker_result[item] = np.NaN
+              
+        # Append results for the current ticker to the final list          
+        results_list.append(ticker_result)
+    
+    return results_list
+
+def extract_data_multiple(query_result_list, selected_items_list, query_time=None):
+    """   
+    Extracts items from a query with yahooquery library. 
+    Returns a list with one dict per ticker in the query containing query_time and selected_items_list datas 
+       
+    Arguments
+    ----------
+    query_result: list containing dicts
+        Result of a queries with yahooquery library with a method applied (ex: yahooquery.Ticker(ticker).financial_data)
+    selected_items: list containing list of str
+        Names of items to extract (ex: [['priceHint', 'previousClose', 'open'], ['regularMarketChangePercent', 'regularMarketChange']])
+    query_time: timestamp
+        Time at which the query has been performed
+    """
+       
+    # Extract datas
+    i = 0
+    
+    for query_result, selected_items in zip(query_result_list, selected_items_list):
+        
+        extract = pd.DataFrame(extract_data_single(query_result, selected_items, query_time))
+        
+        # If it is the first loop we need to create a DataFrame with the extract
+        if i == 0:
+            combined_extract = extract.copy()
+            i += 1
+        
+        # Else we merge the new extract with the existing DataFrame from first loop
+        else:
+            if query_time:
+                combined_extract = pd.merge(combined_extract, extract, on=['symbol', 'date'])
+            else:
+                combined_extract = pd.merge(combined_extract, extract, on=['symbol'])
+    
+    return combined_extract
