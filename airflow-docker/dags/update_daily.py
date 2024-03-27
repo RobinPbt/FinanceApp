531a4cb04f34e4753dcba95c6580b4cd4780bf98
RobinPbt <104992181+RobinPbt@users.noreply.github.com>
2024-03-27 12:13:57 +0100

    Externalize functions

--- airflow-docker/dags/update_daily.py
+++ airflow-docker/dags/update_daily.py
@@ -19,64 +19,11 @@ import pytz
 from yahooquery import Ticker
 # from symbols import CAC_40
 
+from functions import *
+
 # log = logging.getLogger(__name__)
 # PATH_TO_PYTHON_BINARY = sys.executable
 # BASE_DIR = tempfile.gettempdir()
-
-
-def extract_data_single(query_result, selected_items, query_time=None):
-    """
-    Extracts items from a query with yahooquery library. 
-    Returns a list with one dict per ticker in the query containing query_time and selected_items datas 
-       
-    Arguments
-    ----------
-    query_result: dict
-        Result of a query with yahooquery library with a method applied (ex: yahooquery.Ticker(ticker).financial_data)
-    selected_items: list of str
-        Names of items to extract (ex: ['regularMarketPrice', 'regularMarketDayHigh'])
-    query_time: timestamp or None
-        Time at which the query has been performed, adds a column with this information for each row in the query
-    """
-    
-    tickers = list(query_result.keys())   
-    results_list = []
-    
-    # For each ticker extract selected items
-    for ticker in tickers:
-        
-        # Get query result for the current ticker
-        query_result_ticker = query_result[ticker]
-        
-        # Instantiante result with time and ticker
-        if query_time:
-            ticker_result = {'symbol': ticker, 'date': query_time}
-        else:
-            ticker_result = {'symbol': ticker}
-        
-        # Collect name of available items for the ticker (yahooquery doesn't return the same items depending on the ticker)
-        if isinstance(query_result_ticker, str): # If the query doesn't find any results it resturns a string
-            available_items = []
-        
-        else: # Else get names of items returned
-            available_items = query_result_ticker.keys()
-        
-        # Now extract items if available
-        for item in selected_items:
-        
-            # Check if data is available, and append items
-            if item in available_items:
-                ticker_result[item] = query_result_ticker[item]
-
-            # If not available, fill with NaN
-            else:
-                ticker_result[item] = np.NaN
-              
-        # Append results for the current ticker to the final list          
-        results_list.append(ticker_result)
-    
-    return results_list
-
 
 @dag(
     dag_id="update_db_daily",
@@ -218,26 +165,19 @@ def update_db_daily():
         query_prices = """
             INSERT INTO stock_price_daily
             SELECT *
-            FROM temp_prices
+            FROM temp_prices;
         """
 
         query_estimates = """
             INSERT INTO estimates_daily
             SELECT *
-            FROM last_estimates
+            FROM last_estimates;
         """
 
-        try:
-            postgres_hook = PostgresHook(postgres_conn_id="finapp_postgres_conn")
-            conn = postgres_hook.get_conn()
-            cur = conn.cursor()
-            cur.execute(query_prices)
-            cur.execute(query_estimates)
-            conn.commit()
-            return 0
-        except Exception as e:
-            return 1
+        cur.execute(query_prices)
+        cur.execute(query_estimates)
+        conn.commit()
 
-    [create_stock_price_daily_table, create_estimates_daily_table, create_last_estimates_table] >> download_data() >> update_db()
+    [create_stock_price_daily_table, create_temp_prices_table, create_estimates_daily_table, create_last_estimates_table] >> download_data() >> update_db()
 
 dag = update_db_daily()
--- airflow-docker/dags/update_month.py
+++ airflow-docker/dags/update_month.py
@@ -19,98 +19,11 @@ import pytz
 from yahooquery import Ticker
 # from symbols import CAC_40
 
+from functions import *
+
 # log = logging.getLogger(__name__)
 # PATH_TO_PYTHON_BINARY = sys.executable
 # BASE_DIR = tempfile.gettempdir()
-
-def extract_data_single(query_result, selected_items, query_time=None):
-    """
-    Extracts items from a query with yahooquery library. 
-    Returns a list with one dict per ticker in the query containing query_time and selected_items datas 
-       
-    Arguments
-    ----------
-    query_result: dict
-        Result of a query with yahooquery library with a method applied (ex: yahooquery.Ticker(ticker).financial_data)
-    selected_items: list of str
-        Names of items to extract (ex: ['regularMarketPrice', 'regularMarketDayHigh'])
-    query_time: timestamp or None
-        Time at which the query has been performed, adds a column with this information for each row in the query
-    """
-    
-    tickers = list(query_result.keys())   
-    results_list = []
-    
-    # For each ticker extract selected items
-    for ticker in tickers:
-        
-        # Get query result for the current ticker
-        query_result_ticker = query_result[ticker]
-        
-        # Instantiante result with time and ticker
-        if query_time:
-            ticker_result = {'symbol': ticker, 'date': query_time}
-        else:
-            ticker_result = {'symbol': ticker}
-        
-        # Collect name of available items for the ticker (yahooquery doesn't return the same items depending on the ticker)
-        if isinstance(query_result_ticker, str): # If the query doesn't find any results it resturns a string
-            available_items = []
-        
-        else: # Else get names of items returned
-            available_items = query_result_ticker.keys()
-        
-        # Now extract items if available
-        for item in selected_items:
-        
-            # Check if data is available, and append items
-            if item in available_items:
-                ticker_result[item] = query_result_ticker[item]
-
-            # If not available, fill with NaN
-            else:
-                ticker_result[item] = np.NaN
-              
-        # Append results for the current ticker to the final list          
-        results_list.append(ticker_result)
-    
-    return results_list
-
-def extract_data_multiple(query_result_list, selected_items_list, query_time=None):
-    """   
-    Extracts items from a query with yahooquery library. 
-    Returns a list with one dict per ticker in the query containing query_time and selected_items_list datas 
-       
-    Arguments
-    ----------
-    query_result: list containing dicts
-        Result of a queries with yahooquery library with a method applied (ex: yahooquery.Ticker(ticker).financial_data)
-    selected_items: list containing list of str
-        Names of items to extract (ex: [['priceHint', 'previousClose', 'open'], ['regularMarketChangePercent', 'regularMarketChange']])
-    query_time: timestamp
-        Time at which the query has been performed
-    """
-       
-    # Extract datas
-    i = 0
-    
-    for query_result, selected_items in zip(query_result_list, selected_items_list):
-        
-        extract = pd.DataFrame(extract_data_single(query_result, selected_items, query_time))
-        
-        # If it is the first loop we need to create a DataFrame with the extract
-        if i == 0:
-            combined_extract = extract.copy()
-            i += 1
-        
-        # Else we merge the new extract with the existing DataFrame from first loop
-        else:
-            if query_time:
-                combined_extract = pd.merge(combined_extract, extract, on=['symbol', 'date'])
-            else:
-                combined_extract = pd.merge(combined_extract, extract, on=['symbol'])
-    
-    return combined_extract
 
 @dag(
     dag_id="update_db_monthly",
@@ -229,13 +142,9 @@ def update_db_monthly():
 
         query_2 = """DROP TABLE IF EXISTS temp_info;"""
 
-        try:
-            cur.execute(query_1)
-            cur.execute(query_2)
-            conn.commit()
-            return 0
-        except Exception as e:
-            return 1
+        cur.execute(query_1)
+        cur.execute(query_2)
+        conn.commit()
 
     [create_general_information_table, create_temp_info_table] >> download_data() >> update_db()
 
