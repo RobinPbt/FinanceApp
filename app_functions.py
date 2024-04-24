ba004ed73208369425f11c6948715f2dfec85447
RobinPbt <104992181+RobinPbt@users.noreply.github.com>
2024-04-25 00:13:30 +0200

    Add portfolio allocation functions

--- app_functions.py
+++ app_functions.py
@@ -177,3 +177,160 @@ def fetch_wikipedia_data(company_name):
 def fetch_web_search_results(company_name):
     search = DuckDuckGoSearchRun()
     return search.run(company_name)
+
+# ----------------------------- Portfolio allocation functions ---------------------------------
+
+def compute_returns(stock_prices_df, continuous=True):
+    """
+    Compute daily returns for provided stocks prices.
+
+    args:
+    - stock_prices_df (pd.DataFrame): panel of stocks prices over a selected period. The shape must be with symbols / tickers as rows, dates as columns and prices as values
+    - continuous (bool): True to compute continious returns, False for discrete returns
+    """
+    
+    # Dividends not included --> TO BE IMPROVED
+    returns_df = stock_prices_df.copy()
+    
+    if continuous: # Continuous returns
+        for col in returns_df.columns:
+            returns_df[col] = np.log(stock_prices_df[col] / stock_prices_df.shift(1, axis="columns")[col])
+            
+    else: # Discrete returns
+        for col in returns_df.columns:
+            stock_prices_df[col] = stock_prices_df[col].pct_change()
+            
+    return returns_df
+
+def expected_return(weights_vector, returns_vector):
+    """
+    Compute the expected return of a given portfolio allocation.
+    
+    args:
+    - weights_vector (np.array): weights allocated to each stock
+    - returns_vector (np.array): expected returns for each stock
+    """
+    
+    expected_return = weights_vector.T @ returns_vector
+    expected_return = expected_return[0][0]
+    
+    return expected_return
+
+def expected_variance(weights_vector, covariance_matrix):
+    """
+    Compute the expected variance of a given portfolio allocation.
+    
+    args:
+    - weights_vector (np.array): weights allocated to each stock
+    - covariance_matrix (np.array): covariance matrix of stock daily returns
+    """
+    
+    expected_variance = weights_vector.T @ covariance_matrix @ weights_vector
+    expected_variance = expected_variance.values[0][0]
+    
+    return expected_variance
+
+class PortfolioAllocation():
+    """
+    Class allowing to compute best allocations of investments (weights) in a panel of stocks to optmize risk/return couple. 
+    
+    args:
+    - historical_prices (pd.DataFrame): panel of stocks prices over a selected period. The shape must be with symbols / tickers as rows, dates as columns and prices as values
+    - expected_returns (pd.Series): expected returns per symbol / ticker if you have an estimation of expected returns (1 value per ticker). If not use computed historical returns
+    """
+       
+    def __init__(self, historical_prices, expected_returns=pd.Series()):
+        
+        self.symbols = historical_prices.index
+        
+        # Compute historical returns for each date and for the period
+        self.daily_historical_returns = compute_returns(historical_prices, continuous=True).T
+        self.total_historical_returns = self.daily_historical_returns.sum()
+        
+        # Choose expected returns, if empty serie use historical returns
+        if expected_returns.empty:
+            self.returns_vector = np.array(self.total_historical_returns).reshape(-1, 1)
+        else:
+            self.returns_vector = np.array(expected_returns).reshape(-1, 1)         
+        
+        # Compute covariance matrix and its inverse
+        self.covariance_matrix = self.daily_historical_returns.cov()
+        self.inv_covariance_matrix = np.linalg.inv(self.covariance_matrix)
+        
+        # Compute vector of ones and returns vector with ones column
+        self.unit_vector = np.full((len(self.returns_vector), 1), 1)
+        self.returns_unit = np.c_[self.returns_vector, self.unit_vector].T
+        
+        # Compute transormation matrix for efficient frontier --> compute directly in __init__ or in a dedicated function ?
+        a = self.returns_unit @ self.inv_covariance_matrix @ self.returns_unit.T
+        inv_a = np.linalg.inv(a)
+        self.transfo_matrix = self.inv_covariance_matrix @ self.returns_unit.T @ inv_a
+        
+    def compute_GMVP_weights(self, display_results=True):
+        """
+        Compute GMVP (Global minimum variance portfolio) weights allocation.
+        
+        args:
+        - display_results (bool): decide to display or not expected return, variance and volatility
+        """
+    
+        self.GMVP_weights = (self.inv_covariance_matrix @ self.unit_vector) / (self.unit_vector.T @ self.inv_covariance_matrix @ self.unit_vector)
+        self.expected_return_GMVP = expected_return(self.GMVP_weights, self.returns_vector)
+        self.expected_variance_GMVP = expected_variance(self.GMVP_weights, self.covariance_matrix)
+
+        if display_results:
+            print("Expected return GMVP : {:.6f}".format(self.expected_return_GMVP))
+            print("Expected variance GMVP : {:.6f}".format(self.expected_variance_GMVP))
+            print("Expected volatility GMVP : {:.6f}".format(np.sqrt(self.expected_variance_GMVP)))
+        
+    def compute_efficient_portfolio_weights(self, target_return, display_results=True):
+        """
+        Compute portfolio weights allocation which minimizes variance for a given target return.
+        
+        args:
+        - display_results (bool): decide to display or not expected return, variance and volatility
+        """
+
+        self.efficient_portfolio_weights = self.transfo_matrix @ np.array([target_return, 1]).reshape(-1, 1)
+        self.expected_return_efficient_portfolio = expected_return(self.efficient_portfolio_weights, self.returns_vector)
+        self.expected_variance_efficient_portfolio = expected_variance(self.efficient_portfolio_weights, self.covariance_matrix)
+        
+        if display_results:
+            print("Expected return efficient portfolio : {:.6f}".format(self.expected_return_efficient_portfolio))
+            print("Expected variance efficient portfolio : {:.6f}".format(self.expected_variance_efficient_portfolio))
+            print("Expected volatility efficient portfolio : {:.6f}".format(np.sqrt(self.expected_variance_efficient_portfolio)))
+        
+    def plot_efficient_frontier(self):
+        """
+        Plot the line of efficient portfolio in the range [-50%, +50%] in a return/variance chart.
+        """
+        
+        expected_returns = [i/100 for i in range(-50, 50, 1)] # Quid letting range as function parameter ? 
+        expected_variances = []
+        
+        # Compute variances for each return specified in expected_returns
+        for i in expected_returns:
+            weights = self.transfo_matrix @ np.array([i, 1]).reshape(-1, 1)
+            expected_var = expected_variance(weights, self.covariance_matrix)
+            expected_variances.append(expected_var)
+            
+        # Plot the frontier and GMVP
+        fig = plt.figure()
+        
+        plt.plot(expected_variances, expected_returns) # Frontier
+        plt.scatter(self.expected_variance_GMVP, self.expected_return_GMVP) # GMVP
+        plt.text(self.expected_variance_GMVP+(7*(10**-7)), self.expected_return_GMVP-(1*(10**-2)), "GMVP")
+        
+        plt.ylabel("Rentability")
+        plt.xlabel("Variance")
+        plt.show()
+
+    def return_weights(self):
+        """
+        Return efficient portfolio and GMVP weights in two pd.Series with symbol names.
+        """
+
+        clean_efficient_portfolio_weights = pd.Series(data=self.efficient_portfolio_weights.reshape(1, -1)[0], index=self.symbols)
+        clean_GMVP_weights = pd.Series(data=self.GMVP_weights.reshape(1, -1)[0], index=self.symbols)
+
+        return clean_efficient_portfolio_weights, clean_GMVP_weights
