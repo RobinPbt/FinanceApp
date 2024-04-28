3c3ee91386f3126473d22c04172bca669995f1d3
RobinPbt <104992181+RobinPbt@users.noreply.github.com>
2024-04-28 23:35:17 +0200

    Change navigation with inclusion of tabs

--- pages/Estimates.py
+++ pages/Estimates.py
@@ -15,6 +15,10 @@ import pytz
 
 from app_functions import *
 
+# ---------------------------------------------------------------------------------------------------
+# CONFIGURATION
+# ---------------------------------------------------------------------------------------------------
+
 st.set_page_config(
     page_title="Estimates",
     layout = "wide"
@@ -27,7 +31,8 @@ con = st.connection(
     url="postgresql+psycopg2://airflow:airflow@localhost/airflow"
 )
 
-# -----------------------------Define caching functions ---------------------------------
+# ---------------------------------------------------------------------------------------------------
+# Define caching functions
 
 @st.cache_data
 def get_tickers_names():
@@ -85,125 +90,134 @@ def load_estimates():
     global_estimates = pd.DataFrame(query_result)
     return global_estimates
 
-# -----------------------------Define sidebar -------------------------------------------
+# ---------------------------------------------------------------------------------------------------
+# DASHBOARD
+# ---------------------------------------------------------------------------------------------------
 
-# Section selection
-section_selection = st.sidebar.selectbox('Section selection', ["Global view", "Individual ticker view"])
+# ---------------------------------------------------------------------------------------------------
+# Sidebar
 
 # Ticker selection    
 tickers_names = get_tickers_names()
 company_names = tickers_names["shortName"]
-if section_selection == "Individual ticker view":
-    company_selection = st.sidebar.selectbox('Company selection', company_names)
-    ticker_selection = tickers_names[tickers_names["shortName"] == company_selection]["symbol"].values[0]
+company_selection = st.sidebar.selectbox('Company selection', company_names)
+ticker_selection = tickers_names[tickers_names["shortName"] == company_selection]["symbol"].values[0]
 
-# -----------------------------Dashboard ------------------------------------------------
+# ---------------------------------------------------------------------------------------------------
+# Header
 
 # Load datas
 global_estimates = load_estimates()
 
-# Define containers
 header = st.container()
-global_view = st.container()
-detailed_view = st.container()
 
 with header:
+   
+    st.write("""
+    # Estimates
+    Difference between stock prices and the median target price set by analysts.
+    """)
 
-    if section_selection == "Global view":
-        header_text = """
-        # Estimates
-        Difference between stock prices and the median target price set by analysts.
-        """
-    else:
-         header_text = """
-        # Estimates
-        """
+# Create tabs
+tab1, tab2 = st.tabs(["Global view", "Individual ticker view"])
+
+# ---------------------------------------------------------------------------------------------------
+# Global view
+
+with tab1:
+
+    st.write("""## Global view""")
+    all_estimates_section = st.container()
+    top_10_section = st.container()
+    bottom_10_section = st.container()
     
-    st.write(header_text)
-
-if section_selection == "Global view":
-    with global_view:
-        st.write("""## Global view""")
-        all_estimates_section = st.container()
-        top_10_section = st.container()
-        bottom_10_section = st.container()
+    with all_estimates_section:
         
-        with all_estimates_section:
-            # show_estimates = st.checkbox("Show all estimates")
+        # show_estimates = st.checkbox("Show all estimates")
+        # if show_estimates:
+        with st.expander("Show all estimates"):
+            st.dataframe(data=global_estimates[["shortName", "lastPrice", "targetMedianPrice", "EstimatesAbsoluteDiff", "EstimatesRelativeDiff", "EstimatesConfidence"]])
 
-            # if show_estimates:
-            with st.expander("Show all estimates"):
-                st.dataframe(data=global_estimates[["shortName", "lastPrice", "targetMedianPrice", "EstimatesAbsoluteDiff", "EstimatesRelativeDiff", "EstimatesConfidence"]])
+    with top_10_section:
+        top_10 = global_estimates[["shortName", "EstimatesRelativeDiff"]].dropna(subset="EstimatesRelativeDiff").sort_values(by=["EstimatesRelativeDiff"], ascending=False).head(10)
 
-        with top_10_section:
-            top_10 = global_estimates[["shortName", "EstimatesRelativeDiff"]].dropna(subset="EstimatesRelativeDiff").sort_values(by=["EstimatesRelativeDiff"], ascending=False).head(10)
+        # Plot chart with top 10 undervalued stocks
+        fig = px.bar(
+            data_frame=top_10,
+            x="shortName", 
+            y="EstimatesRelativeDiff", 
+            title="Top 10 undervalued stocks",
+            labels={"shortName" : "", "EstimatesRelativeDiff" : "Percentage (+)under/(-)over valuation"}
+        )
 
-            # Plot chart with top 10 undervalued stocks
-            fig = px.bar(
-                data_frame=top_10,
-                x="shortName", 
-                y="EstimatesRelativeDiff", 
-                title="Top 10 undervalued stocks",
-                labels={"shortName" : "", "EstimatesRelativeDiff" : "Percentage (+)under/(-)over valuation"}
-            )
+        st.plotly_chart(fig, use_container_width=True)
 
-            st.plotly_chart(fig, use_container_width=True)
+    with bottom_10_section:
+        bottom_10 = global_estimates[["shortName", "EstimatesRelativeDiff"]].dropna(subset="EstimatesRelativeDiff").sort_values(by=["EstimatesRelativeDiff"], ascending=True).head(10)
 
-        with bottom_10_section:
-            bottom_10 = global_estimates[["shortName", "EstimatesRelativeDiff"]].dropna(subset="EstimatesRelativeDiff").sort_values(by=["EstimatesRelativeDiff"], ascending=True).head(10)
+        # Plot chart with top 10 undervalued stocks
+        fig = px.bar(
+            data_frame=bottom_10,
+            x="shortName", 
+            y="EstimatesRelativeDiff", 
+            title="Top 10 overvalued stocks",
+            labels={"shortName" : "", "EstimatesRelativeDiff" : "Percentage (+)under/(-)over valuation"}
+        )
 
-            # Plot chart with top 10 undervalued stocks
-            fig = px.bar(
-                data_frame=bottom_10,
-                x="shortName", 
-                y="EstimatesRelativeDiff", 
-                title="Top 10 overvalued stocks",
-                labels={"shortName" : "", "EstimatesRelativeDiff" : "Percentage (+)under/(-)over valuation"}
-            )
+        st.plotly_chart(fig, use_container_width=True)
 
-            st.plotly_chart(fig, use_container_width=True)
+# ---------------------------------------------------------------------------------------------------
+# Detailled view
 
-elif section_selection == "Individual ticker view":
-    with detailed_view:
+with tab2:
         
-        # Load datas from current selection
-        selected_estimates = global_estimates[global_estimates['symbol'] == ticker_selection]
+    # Load datas from current selection
+    selected_estimates = global_estimates[global_estimates['symbol'] == ticker_selection]
 
-        x_axis_price = ['targetMedianPrice', 'lastPrice']
-        current_price = selected_estimates[x_axis_price]
-        current_price.columns = ['Selected estimates price', 'Current price']
+    x_axis_price = ['targetMedianPrice', 'lastPrice']
+    current_price = selected_estimates[x_axis_price]
+    current_price.columns = ['Selected estimates price', 'Current price']
 
-        x_axis_estimates = ["targetLowPrice", "targetMeanPrice", "targetMedianPrice", "targetHighPrice"]
-        current_estimates = selected_estimates[x_axis_estimates]
+    x_axis_estimates = ["targetLowPrice", "targetMeanPrice", "targetMedianPrice", "targetHighPrice"]
+    current_estimates = selected_estimates[x_axis_estimates]
 
-        # Display
-        st.write("""## Selected company detailed view""")
+    # Display
+    st.write("""## Selected company detailed view""")
 
-        detailed_view_col1, detailed_view_col2, detailed_view_col3 = st.columns(3)
+    detailed_view_col1, detailed_view_col2, detailed_view_col3 = st.columns(3)
 
-        with detailed_view_col1:
-            st.write("Company name: {}".format(selected_estimates['shortName'].values[0]))
-        with detailed_view_col2: 
-            st.write("Relative difference with current price: {:.2%}".format(selected_estimates['EstimatesRelativeDiff'].values[0]))
-        with detailed_view_col3:
-            st.write("Confidence estimates: {}".format(selected_estimates['EstimatesConfidence'].values[0]))
+    with detailed_view_col1:
+        st.metric(
+            label="Company name", 
+            value=selected_estimates['shortName'].values[0]
+        )
+    with detailed_view_col2: 
+        st.metric(
+            label="Difference with current price", 
+            value=metrics_value_formatting(selected_estimates['EstimatesRelativeDiff'].values[0], value_type="percentage", percentage_format=""), 
+        )
+    with detailed_view_col3:
+        st.metric(
+            label="Confidence estimates", 
+            value=selected_estimates['EstimatesConfidence'].values[0]
+        )
 
-        graph_estimates, compare_graph = st.columns(2)
+    graph_estimates, compare_graph = st.columns(2)
 
-        with graph_estimates:
-           fig = px.bar(
-                x=current_estimates.values[0], 
-                y=current_estimates.columns, 
-                title="Estimates distribution", 
-                labels={"x" : "", "y" : ""},
-            )
-           st.plotly_chart(fig)        
-        
-        with compare_graph:
-            fig = px.bar(
-                x=current_price.columns, 
-                y=current_price.values[0], 
-                title="Compare valuation against current price", 
-                labels={"x" : "", "y" : ""}
-            )
-            st.plotly_chart(fig)
+    with graph_estimates:
+        fig = px.bar(
+            x=current_estimates.values[0], 
+            y=current_estimates.columns, 
+            title="Estimates distribution", 
+            labels={"x" : "", "y" : ""},
+        )
+        st.plotly_chart(fig)        
+    
+    with compare_graph:
+        fig = px.bar(
+            x=current_price.columns, 
+            y=current_price.values[0], 
+            title="Compare valuation against current price", 
+            labels={"x" : "", "y" : ""}
+        )
+        st.plotly_chart(fig)
--- pages/Listed_peers.py
+++ pages/Listed_peers.py
@@ -16,6 +16,10 @@ import pytz
 
 from app_functions import *
 
+# ---------------------------------------------------------------------------------------------------
+# CONFIGURATION
+# ---------------------------------------------------------------------------------------------------
+
 st.set_page_config(
     page_title="Listed Peers",
     layout = "wide"
@@ -28,7 +32,8 @@ con = st.connection(
     url="postgresql+psycopg2://airflow:airflow@localhost/airflow"
 )
 
-# -----------------------------Define caching functions ---------------------------------
+# ---------------------------------------------------------------------------------------------------
+# Define caching functions
 
 @st.cache_data
 def get_tickers_names():
@@ -103,199 +108,215 @@ def create_bar_chart(sector_multiples, multiple, title):
 
     return fig
 
-# -----------------------------Define sidebar -------------------------------------------
+# ---------------------------------------------------------------------------------------------------
+# DASHBOARD
+# ---------------------------------------------------------------------------------------------------
 
-# Section selection
-section_selection = st.sidebar.selectbox('Section selection', ["Global view", "Sector multiples", "Individual ticker view"])
+# ---------------------------------------------------------------------------------------------------
+# Sidebar
 
 # Ticker selection    
 tickers_names = get_tickers_names()
 company_names = tickers_names["shortName"]
-if section_selection == "Individual ticker view":
-    company_selection = st.sidebar.selectbox('Company selection', company_names)
-    ticker_selection = tickers_names[tickers_names["shortName"] == company_selection]["symbol"].values[0]
+company_selection = st.sidebar.selectbox('Company selection', company_names)
+ticker_selection = tickers_names[tickers_names["shortName"] == company_selection]["symbol"].values[0]
 
-# -----------------------------Dashboard ------------------------------------------------
+# ---------------------------------------------------------------------------------------------------
+# Header
 
 # Load datas
 peers = get_peers()
 
 # Define containers
 header = st.container()
-global_view = st.container()
-multiples_view = st.container()
-detailed_view = st.container()
+# global_view = st.container()
+# multiples_view = st.container()
+# detailed_view = st.container()
 
 with header:
+       
+    st.write("""
+    # Listed Peers
+    Difference between stock prices and the price estimated with a traditional peers valuation approach by sector.
+    """)
+
+# Create tabs
+tab1, tab2, tab3 = st.tabs(["Global view", "Mean sector multiples", "Individual ticker view"])
+
+# ---------------------------------------------------------------------------------------------------
+# Global view
+
+with tab1:
+
+    st.write("""## Global view""")
+    st.write("""
+    Below are the main steps for the valuation approach:
+    - We compute multiples for each company (revenue, ebitda, price to book, price earnings)
+    - We compute mean multiples per sector by aggregating previous results
+    - We compute the theoritical valuation for each company by applying mean sector multiples to company financial items
+    - We compute the difference between the current price and the theoritical valuation """)
+
+    all_peers_section = st.container()
+    top_10_section = st.container()
+    bottom_10_section = st.container()
+
+    with all_peers_section:
+        
+        # show_peers = st.checkbox("Show all peers")
+        # if show_peers:
+        with st.expander("Show all peers"):
+            st.dataframe(
+                data=peers[[
+                    'shortName',
+                    'sector',
+                    'lastPrice',
+                    'date', 
+                    'PeersMeanStockPrice', 
+                    'PeersRelativeStdStockPrice', 
+                    'PeersAbsoluteDiff', 
+                    'PeersRelativeDiff', 
+                    'PeersConfidence'
+                ]]
+            )
+
+    with top_10_section:
+        top_10 = peers[["shortName", "PeersRelativeDiff"]].dropna(subset="PeersRelativeDiff").sort_values(by=["PeersRelativeDiff"], ascending=False).head(10)
+
+        # Plot chart with top 10 undervalued stocks
+        fig = px.bar(
+            data_frame=top_10,
+            x="shortName", 
+            y="PeersRelativeDiff", 
+            title="Top 10 undervalued stocks",
+            labels={"shortName" : "", "PeersRelativeDiff" : "Percentage (+)under/(-)over valuation"}
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with bottom_10_section:
+        bottom_10 = peers[["shortName", "PeersRelativeDiff"]].dropna(subset="PeersRelativeDiff").sort_values(by=["PeersRelativeDiff"], ascending=True).head(10)
+
+        # Plot chart with top 10 undervalued stocks
+        fig = px.bar(
+            data_frame=bottom_10,
+            x="shortName", 
+            y="PeersRelativeDiff", 
+            title="Top 10 overvalued stocks",
+            labels={"shortName" : "", "PeersRelativeDiff" : "Percentage (+)under/(-)over valuation"}
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+# ---------------------------------------------------------------------------------------------------
+# Sector multiples
+
+with tab2:
+    # Load data
+    sector_multiples = get_sector_multiples()
+    sector_multiples = sector_multiples.sort_values(by="sector", ascending=False)
+
+    # Section title
+    st.write("""## Mean sector multiples""")
+
+    price_to_book_col, enterprise_to_revenue_col, enterprise_to_ebitda_col, trailing_PE_col = st.columns([1, 1, 1, 1])
+
+    with price_to_book_col:
+        fig = create_bar_chart(sector_multiples, "MeanSectorPriceToBook", "Price to Book")                
+        st.plotly_chart(fig, use_container_width=True)
+    with enterprise_to_revenue_col:
+        fig = create_bar_chart(sector_multiples, "MeanSectorEnterpriseToRevenue", "Enterprise to Revenue")                
+        st.plotly_chart(fig, use_container_width=True)
+    with enterprise_to_ebitda_col:
+        fig = create_bar_chart(sector_multiples, "MeanSectorEnterpriseToEbitda", "Enterprise to Ebitda")                
+        st.plotly_chart(fig, use_container_width=True)
+    with trailing_PE_col:
+        fig = create_bar_chart(sector_multiples, "MeanSectorTrailingPE", "Trailing PE")                
+        st.plotly_chart(fig, use_container_width=True)
+
+# ---------------------------------------------------------------------------------------------------
+# Individual ticker view
+
+with tab3:
+    # Load datas from current selection
+    selected_peers = peers[peers['symbol'] == ticker_selection]
+    sector_multiples = get_sector_multiples()
+    selected_sector_multiples = sector_multiples[sector_multiples["sector"] == selected_peers['sector'].values[0]].drop("sector", axis=1)
     
-    if section_selection == "Global view":
-        header_text = """
-        # Listed Peers
-        Difference between stock prices and the price estimated with a traditional peers valuation approach by sector.
-        Below are the main steps for this approach:
-        - We compute multiples for each company (revenue, ebitda, price to book, price earnings)
-        - We compute mean multiples per sector by aggregating previous results
-        - We compute the theoritical valuation for each company by applying mean sector multiples to company financial items
-        - We compute the difference between the current price and the theoritical valuation 
-        """
-    else:
-         header_text = """
-        # Listed Peers
-        """
+    x_axis_peers = ['stockPriceBook', 'stockPriceRevenue', 'stockPriceEbitda', 'stockPriceEarnings']
+    current_multiples = selected_peers[x_axis_peers]
+
+    x_axis_price = ['PeersMeanStockPrice', 'lastPrice']
+    current_price = selected_peers[x_axis_price]
+    current_price.columns = ['Valuation price', 'Current price']
+
+    # Display
+    st.write("""## Selected ticker detailed view""")
     
-    st.write(header_text)
+    detailed_view_col1, detailed_view_col2, detailed_view_col3, detailed_view_col4 = st.columns(4)
 
-if section_selection == "Global view":
-    with global_view:
-        st.write("""## Global view""")
-        all_peers_section = st.container()
-        top_10_section = st.container()
-        bottom_10_section = st.container()
+    with detailed_view_col1:
+        st.metric(
+            label="Company name", 
+            value=selected_peers['shortName'].values[0]
+        )
+    with detailed_view_col2:
+        st.metric(
+            label="Difference with current price", 
+            value=metrics_value_formatting(selected_peers['PeersRelativeDiff'].values[0], value_type="percentage", percentage_format=""), 
+        )    
+    with detailed_view_col3: 
+        st.metric(
+            label="Sector", 
+            value=selected_peers['sector'].values[0]
+        )
+    with detailed_view_col4:
+        st.metric(
+            label="Confidence valuation", 
+            value=selected_peers['PeersConfidence'].values[0]
+        )
 
-        with all_peers_section:
-            # show_peers = st.checkbox("Show all peers")
+    graph_multiples, compare_graph = st.columns(2)
 
-            # if show_peers:
-            with st.expander("Show all peers"):
-                st.dataframe(
-                    data=peers[[
-                        'shortName',
-                        'sector',
-                        'lastPrice',
-                        'date', 
-                        'PeersMeanStockPrice', 
-                        'PeersRelativeStdStockPrice', 
-                        'PeersAbsoluteDiff', 
-                        'PeersRelativeDiff', 
-                        'PeersConfidence'
-                    ]]
-                )
+    with graph_multiples:
+        # Plot graph with stock prices
+        fig = px.bar(
+            x=current_multiples.columns, y=current_multiples.values[0], 
+            title="Stock price per valuation method", labels={"x" : "", "y" : ""}
+        )
+        # Add a horizontal line with the mean
+        fig.add_shape(
+            type="line", line_color="red", line_width=3, opacity=1, line_dash="dot",
+            x0=0, x1=1, xref="paper", y0=current_price['Valuation price'].values[0], y1=current_price['Valuation price'].values[0], yref="y"
+        )
+        st.plotly_chart(fig)
 
-        with top_10_section:
-            top_10 = peers[["shortName", "PeersRelativeDiff"]].dropna(subset="PeersRelativeDiff").sort_values(by=["PeersRelativeDiff"], ascending=False).head(10)
+    with compare_graph:
+        fig = px.bar(
+            x=current_price.columns, 
+            y=current_price.values[0], 
+            title="Compare valuation against current price", 
+            labels={"x" : "", "y" : ""}
+        )
+        st.plotly_chart(fig)
 
-            # Plot chart with top 10 undervalued stocks
-            fig = px.bar(
-                data_frame=top_10,
-                x="shortName", 
-                y="PeersRelativeDiff", 
-                title="Top 10 undervalued stocks",
-                labels={"shortName" : "", "PeersRelativeDiff" : "Percentage (+)under/(-)over valuation"}
-            )
+    multiple_compare = st.container()
 
-            st.plotly_chart(fig, use_container_width=True)
-
-        with bottom_10_section:
-            bottom_10 = peers[["shortName", "PeersRelativeDiff"]].dropna(subset="PeersRelativeDiff").sort_values(by=["PeersRelativeDiff"], ascending=True).head(10)
-
-            # Plot chart with top 10 undervalued stocks
-            fig = px.bar(
-                data_frame=bottom_10,
-                x="shortName", 
-                y="PeersRelativeDiff", 
-                title="Top 10 overvalued stocks",
-                labels={"shortName" : "", "PeersRelativeDiff" : "Percentage (+)under/(-)over valuation"}
-            )
-
-            st.plotly_chart(fig, use_container_width=True)
-
-elif section_selection == "Sector multiples":
-    with multiples_view:
-        # Load data
-        sector_multiples = get_sector_multiples()
-        sector_multiples = sector_multiples.sort_values(by="sector", ascending=False)
-
-        # Section title
-        st.write("""## Mean sector multiples""")
-
-        price_to_book_col, enterprise_to_revenue_col, enterprise_to_ebitda_col, trailing_PE_col = st.columns([1, 1, 1, 1])
-
-        with price_to_book_col:
-            fig = create_bar_chart(sector_multiples, "MeanSectorPriceToBook", "Price to Book")                
-            st.plotly_chart(fig, use_container_width=True)
-        with enterprise_to_revenue_col:
-            fig = create_bar_chart(sector_multiples, "MeanSectorEnterpriseToRevenue", "Enterprise to Revenue")                
-            st.plotly_chart(fig, use_container_width=True)
-        with enterprise_to_ebitda_col:
-            fig = create_bar_chart(sector_multiples, "MeanSectorEnterpriseToEbitda", "Enterprise to Ebitda")                
-            st.plotly_chart(fig, use_container_width=True)
-        with trailing_PE_col:
-            fig = create_bar_chart(sector_multiples, "MeanSectorTrailingPE", "Trailing PE")                
-            st.plotly_chart(fig, use_container_width=True)
-
-elif section_selection == "Individual ticker view":
-    with detailed_view:
-        # Load datas from current selection
-        selected_peers = peers[peers['symbol'] == ticker_selection]
-        sector_multiples = get_sector_multiples()
-        selected_sector_multiples = sector_multiples[sector_multiples["sector"] == selected_peers['sector'].values[0]].drop("sector", axis=1)
+    with multiple_compare:
         
-        x_axis_peers = ['stockPriceBook', 'stockPriceRevenue', 'stockPriceEbitda', 'stockPriceEarnings']
-        current_multiples = selected_peers[x_axis_peers]
+        x_axis_multiples = ["priceToBook", "enterpriseToRevenue", "enterpriseToEbitda", "trailingPE"]
 
-        x_axis_price = ['PeersMeanStockPrice', 'lastPrice']
-        current_price = selected_peers[x_axis_price]
-        current_price.columns = ['Valuation price', 'Current price']
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            x=x_axis_multiples,
+            y=selected_peers[x_axis_multiples].values[0],
+            name='Company multiples',
+        ))
+        fig.add_trace(go.Bar(
+            x=x_axis_multiples,
+            y=selected_sector_multiples.values[0],
+            name='Sector multiples',
+            # marker_color='indianred'
+        ))
+        fig.update_layout(title_text='Compare company multiples against sector')
 
-        # Display
-        st.write("""## Selected ticker detailed view""")
-        
-        detailed_view_col1, detailed_view_col2 = st.columns(2)
-
-        with detailed_view_col1:
-            st.write("Company name: {}".format(selected_peers['shortName'].values[0]))
-        with detailed_view_col2:
-            st.write("Relative difference with current price: {:.2%}".format(selected_peers['PeersRelativeDiff'].values[0]))
-        
-        detailed_view_col3, detailed_view_col4 = st.columns(2)
-        
-        with detailed_view_col3: 
-            st.write("Sector: {}".format(selected_peers['sector'].values[0]))
-        with detailed_view_col4:
-            st.write("Confidence valuation: {}".format(selected_peers['PeersConfidence'].values[0]))
-
-        graph_multiples, compare_graph = st.columns(2)
-
-        with graph_multiples:
-            # Plot graph with stock prices
-            fig = px.bar(
-                x=current_multiples.columns, y=current_multiples.values[0], 
-                title="Stock price per valuation method", labels={"x" : "", "y" : ""}
-            )
-            # Add a horizontal line with the mean
-            fig.add_shape(
-                type="line", line_color="red", line_width=3, opacity=1, line_dash="dot",
-                x0=0, x1=1, xref="paper", y0=current_price['Valuation price'].values[0], y1=current_price['Valuation price'].values[0], yref="y"
-            )
-            st.plotly_chart(fig)
-
-        with compare_graph:
-            fig = px.bar(
-                x=current_price.columns, 
-                y=current_price.values[0], 
-                title="Compare valuation against current price", 
-                labels={"x" : "", "y" : ""}
-            )
-            st.plotly_chart(fig)
-
-        multiple_compare = st.container()
-
-        with multiple_compare:
-            
-            x_axis_multiples = ["priceToBook", "enterpriseToRevenue", "enterpriseToEbitda", "trailingPE"]
-
-            fig = go.Figure()
-            fig.add_trace(go.Bar(
-                x=x_axis_multiples,
-                y=selected_peers[x_axis_multiples].values[0],
-                name='Company multiples',
-            ))
-            fig.add_trace(go.Bar(
-                x=x_axis_multiples,
-                y=selected_sector_multiples.values[0],
-                name='Sector multiples',
-                # marker_color='indianred'
-            ))
-            fig.update_layout(title_text='Compare company multiples against sector')
-
-            st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig, use_container_width=True)
